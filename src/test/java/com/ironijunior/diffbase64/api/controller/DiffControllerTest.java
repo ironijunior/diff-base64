f7ad0efc13addb1fab3948feacf3c1ead21ab580
ironijunior <ironimedina@gmail.com>
2019-11-04 17:51:20 -0300

    add initial tests for services

--- src/test/java/com/ironijunior/diffbase64/api/controller/DiffControllerTest.java
+++ src/test/java/com/ironijunior/diffbase64/api/controller/DiffControllerTest.java
@@ -1,4 +1,18 @@
 package com.ironijunior.diffbase64.api.controller;
 
+import com.ironijunior.diffbase64.api.service.DiffService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
 public class DiffControllerTest {
+
+    private DiffService service = Mockito.mock(DiffService.class);
+    private DiffController controller;
+
+    @BeforeEach
+    public void setup() {
+        controller = new DiffController(service);
+    }
+
 }
--- src/test/java/com/ironijunior/diffbase64/api/service/impl/DiffServiceImplTest.java
+++ src/test/java/com/ironijunior/diffbase64/api/service/impl/DiffServiceImplTest.java
@@ -1,4 +1,39 @@
 package com.ironijunior.diffbase64.api.service.impl;
 
+import com.ironijunior.diffbase64.api.repository.DiffRepository;
+import com.ironijunior.diffbase64.api.service.DiffService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class DiffServiceImplTest {
+
+    private DiffRepository repository = Mockito.mock(DiffRepository.class);
+    private DiffService service;
+
+    @BeforeEach
+    public void setup() {
+        service = new DiffServiceImpl(repository);
+    }
+
+    @Test
+    public void convertByteArrayToStringTest() {
+        String expected = "This is a string transformed in a byte array.";
+        assertEquals(expected, service.convertByteArrayToString(expected.getBytes()));
+    }
+
+    @Test
+    public void convertNullByteArrayToStringTest() {
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> service.convertByteArrayToString(null));
+    }
+
+    @Test
+    public void convertEmptyByteArrayToStringTest() {
+        Assertions.assertTrue(service.convertByteArrayToString(new byte[0]).isEmpty());
+    }
+
 }
