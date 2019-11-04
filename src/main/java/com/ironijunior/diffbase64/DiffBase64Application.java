323ed7f0194982d94b1024f40e9fa2db2f594fc6
ironijunior <ironimedina@gmail.com>
2019-11-04 15:27:09 -0300

    add initial structure

---
+++ src/main/java/com/ironijunior/diffbase64/DiffBase64Application.java
@@ -0,0 +1,13 @@
+package com.ironijunior.diffbase64;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiffBase64Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiffBase64Application.class, args);
+	}
+
+}
