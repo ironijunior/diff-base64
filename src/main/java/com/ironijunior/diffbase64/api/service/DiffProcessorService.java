5c93e7ba1595189a15a8dd4829417ff26137be5d
ironijunior <ironimedina@gmail.com>
2019-11-05 17:50:30 -0300

    implement diff process and refine the tests

--- src/main/java/com/ironijunior/diffbase64/api/service/DiffProcessorService.java
+++ src/main/java/com/ironijunior/diffbase64/api/service/DiffProcessorService.java
@@ -1,8 +1,24 @@
 package com.ironijunior.diffbase64.api.service;
 
+import com.ironijunior.diffbase64.domain.entity.DifferedData;
+
 /**
  * Service class responsible for doing the diff process between
  * the left and right values.
  */
 public interface DiffProcessorService {
+
+    /**
+     * Process and saves the diff between both sides of the object {@code data}.
+     *
+     * - If the objects are equal it saves the status as "Objects are equal".
+     * - If the objects have different sizes it saves the status as "Objects are different in size".
+     * - If the objects have the same size but are not equal, it saves the status "Objects are different"
+     * and also saves the list of offsets where are the differences.
+     *
+     * @param data DifferedData object to be compared
+     * @return The object saved.
+     */
+    DifferedData diffSides(DifferedData data);
+
 }
--- src/main/java/com/ironijunior/diffbase64/api/service/impl/DiffProcessorServiceImpl.java
+++ src/main/java/com/ironijunior/diffbase64/api/service/impl/DiffProcessorServiceImpl.java
@@ -2,16 +2,79 @@ package com.ironijunior.diffbase64.api.service.impl;
 
 import com.ironijunior.diffbase64.api.repository.DiffRepository;
 import com.ironijunior.diffbase64.api.service.DiffProcessorService;
+import com.ironijunior.diffbase64.domain.entity.DifferedData;
+import com.ironijunior.diffbase64.domain.entity.DifferenceData;
+import com.ironijunior.diffbase64.domain.enumerator.DiffStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class DiffProcessorServiceImpl implements DiffProcessorService {
 
-    private DiffRepository repository;
+    private DiffRepository diffRepository;
 
     @Autowired
     public DiffProcessorServiceImpl(DiffRepository repository) {
-        this.repository = repository;
+        this.diffRepository = repository;
+    }
+
+    private static DifferenceData getNewDifference(long initialOffset, long length) {
+        return new DifferenceData(initialOffset, initialOffset + length, length);
+    }
+
+    @Override
+    public DifferedData diffSides(DifferedData data) {
+        DifferedData.DifferedDataBuilder diffBuilder = DifferedData.builder()
+                .id(data.getId())
+                .left(data.getLeft())
+                .right(data.getRight());
+
+        DiffStatus status = getDiffStatus(data);
+        diffBuilder.status(status.getText());
+
+        if (DiffStatus.DIFFERENT.equals(status)) {
+            diffBuilder.differences(getDifferencesList(data.getLeft(), data.getRight()));
+        }
+        return diffRepository.save(diffBuilder.build());
+    }
+
+    private DiffStatus getDiffStatus(DifferedData data) {
+        if (data.getLeft().equals(data.getRight())) {
+            return DiffStatus.EQUALS;
+        } else if (data.getLeft().length() != data.getRight().length()) {
+            return DiffStatus.DIFFERENT_SIZES;
+        }
+        return DiffStatus.DIFFERENT;
+    }
+
+    private List<DifferenceData> getDifferencesList(String left, String right) {
+        List<DifferenceData> differences = new ArrayList<>();
+
+        long initialOffset = 0;
+        long length = 0;
+        boolean isDifferent = false;
+
+        for (int i = 0; i < left.length(); i++) {
+            if (left.charAt(i) != right.charAt(i)) {
+                if (!isDifferent) {
+                    initialOffset = i;
+                }
+                length += 1;
+                isDifferent = true;
+            } else {
+                if (isDifferent) {
+                    differences.add(getNewDifference(initialOffset, length));
+                    isDifferent = false;
+                    length = 0;
+                }
+            }
+        }
+        if (isDifferent) {
+            differences.add(getNewDifference(initialOffset, length));
+        }
+        return differences;
     }
 }
