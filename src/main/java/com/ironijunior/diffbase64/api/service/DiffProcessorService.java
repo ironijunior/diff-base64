5415da13bef5c905fb9f4ca4c527f680f8d986ea
ironijunior <ironimedina@gmail.com>
2019-11-05 16:09:54 -0300

    add javadoc to the interface

--- src/main/java/com/ironijunior/diffbase64/api/service/DiffProcessorService.java
+++ src/main/java/com/ironijunior/diffbase64/api/service/DiffProcessorService.java
@@ -1,4 +1,8 @@
 package com.ironijunior.diffbase64.api.service;
 
+/**
+ * Service class responsible for doing the diff process between
+ * the left and right values.
+ */
 public interface DiffProcessorService {
 }
