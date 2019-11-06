9eef03d4081d944f2aa46621fa8e59925ebedf82
ironijunior <ironimedina@gmail.com>
2019-11-06 18:31:34 -0300

    fix possible NPE

--- src/main/java/com/ironijunior/diffbase64/transport/dto/DiffResponseDTO.java
+++ src/main/java/com/ironijunior/diffbase64/transport/dto/DiffResponseDTO.java
@@ -1,32 +1,40 @@
 package com.ironijunior.diffbase64.transport.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.ironijunior.diffbase64.domain.DifferedData;
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.util.CollectionUtils;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Data
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class DiffResponseDTO {
 
     private String status;
     private List<DifferenceDTO> differences;
 
     public static DiffResponseDTO convertFromEntity(DifferedData diffData) {
-        return DiffResponseDTO.builder()
-                .status(diffData.getStatus())
-                .differences(
-                        diffData.getDifferences()
-                                .stream()
-                                .map(diff -> DifferenceDTO.builder()
-                                        .initialOffset(diff.getInitialOffset())
-                                        .finalOffset(diff.getFinalOffset())
-                                        .length(diff.getLength())
-                                        .build())
-                                .collect(Collectors.toList())
-                )
-                .build();
+        DiffResponseDTO.DiffResponseDTOBuilder builder = DiffResponseDTO.builder()
+                .status(diffData.getStatus());
+
+        if (!CollectionUtils.isEmpty(diffData.getDifferences())) {
+            builder.differences(convertDifferences(diffData));
+        }
+        return builder.build();
+    }
+
+    private static List<DifferenceDTO> convertDifferences(DifferedData diffData) {
+        return diffData.getDifferences()
+                .stream()
+                .map(diff -> DifferenceDTO.builder()
+                        .initialOffset(diff.getInitialOffset())
+                        .finalOffset(diff.getFinalOffset())
+                        .length(diff.getLength())
+                        .build())
+                .collect(Collectors.toList());
     }
 }
