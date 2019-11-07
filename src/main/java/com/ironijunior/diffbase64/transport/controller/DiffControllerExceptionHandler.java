7ea451d1ed3c2b99c9857647c2ec759bc0060aa5
ironijunior <ironimedina@gmail.com>
2019-11-06 21:08:34 -0300

    refactor exception handler & add logger

--- src/main/java/com/ironijunior/diffbase64/transport/controller/DiffControllerExceptionHandler.java
+++ src/main/java/com/ironijunior/diffbase64/transport/controller/DiffControllerExceptionHandler.java
@@ -3,6 +3,8 @@ package com.ironijunior.diffbase64.transport.controller;
 import com.ironijunior.diffbase64.domain.exception.EntityNotFoundException;
 import com.ironijunior.diffbase64.domain.exception.SideAlreadyFilledException;
 import com.ironijunior.diffbase64.transport.dto.ErrorResponseDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,24 +16,36 @@ import java.util.Locale;
 @ControllerAdvice
 public class DiffControllerExceptionHandler {
 
+    private static final Logger logger = LoggerFactory.getLogger(DiffControllerExceptionHandler.class);
+
     @ExceptionHandler(EntityNotFoundException.class)
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
     @ResponseBody
     public ErrorResponseDTO handleEntityNotFoundException(EntityNotFoundException ex, Locale locale) {
-        return ErrorResponseDTO.builder()
-                .status(HttpStatus.NOT_FOUND.value())
-                .message(ex.getMessage())
-                .build();
+        return createAndLogErrorResponse(ex, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(SideAlreadyFilledException.class)
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     @ResponseBody
     public ErrorResponseDTO handleSideAlreadyFilledException(SideAlreadyFilledException ex, Locale locale) {
-        return ErrorResponseDTO.builder()
-                .status(HttpStatus.BAD_REQUEST.value())
+        return createAndLogErrorResponse(ex, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    public ErrorResponseDTO handleUnexpectedException(Exception ex, Locale locale) {
+        return createAndLogErrorResponse(ex, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    private ErrorResponseDTO createAndLogErrorResponse(Exception ex, HttpStatus status) {
+        ErrorResponseDTO error = ErrorResponseDTO.builder()
+                .status(status.value())
                 .message(ex.getMessage())
                 .build();
+        logger.error(error.getMessage(), error);
+        return error;
     }
 
 }
